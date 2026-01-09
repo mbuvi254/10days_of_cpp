9023d2404d1b2b86acbd44f7df8d2a87159ee936
mbuvi254 <danielmulatya95@gmail.com>
2026-01-09 10:43:18 +0300

    Outputs

--- src/output.cpp
+++ src/output.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-using namespace std;
+
 
 int main() {
-  cout << "Hello World!";
+  std::cout << "Hello World! \n";
+  std:: cout << "C++ Course " << std::endl; //new line manipulator
+  
   return 0;
 }
+// The cout object, together with the << operator, is used to output values/print text:
+// You can add as many cout objects as you want. However, note that it does not insert a new line at the end of the output:
+// \n adds new line , two \n add a blank line 
+// \n The newline character (\n) is called an escape sequence, and it forces the cursor 
+// to change its position to the beginning of the next line on the screen.This results in a new line.
+// \t creates a horizontal tab
+// \\ Inserts a backslash character (\)
+// \"  Inserts a double quote character
+
+// this is a single comment
+// Multi-line comments start with /* and ends with */.
+
+
+
+
+
